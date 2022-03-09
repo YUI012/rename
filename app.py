02f813d4e932192ed516fcee73dce6b27bbefc7e
YUI012 <yxiaolin966@gmail.com>
2022-03-10 01:46:14 +0900

    re342

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
 from flask import Flask,request,render_template,redirect
 import qrcode
-import os
-import shutil
 
 app=Flask(__name__)
 
 @app.route('/')
 def index():
-    shutil.rmtree('static')
-    os.makedir('static')
     return render_template('try_c.html')
 
 @app.route('/try',methods=['POST'])
@@ -20,4 +16,4 @@ def try_c():
     img.save(qr_route)
     return render_template('try_o.html',msg=msg,qr_name=str(name)+'.png',qr_route=qr_route)
 if __name__=='__main__':
-    app.run()
+    app.run(host='0.0.0.0')
