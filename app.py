4d8ab974715b8837c4d5ec4624d3111fe8286176
YUI012 <yxiaolin966@gmail.com>
2022-03-10 00:33:49 +0900

    ver_1.0

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from django.shortcuts import redirect
 from flask import Flask,request,render_template,redirect
 import qrcode
+import os
+import shutil
+
 app=Flask(__name__)
 
 @app.route('/')
 def index():
+    shutil.rmtree('static')
+    os.mkdir('static')
     return render_template('try_c.html')
 
 @app.route('/try',methods=['POST'])
@@ -16,4 +21,4 @@ def try_c():
     img.save(qr_route)
     return render_template('try_o.html',msg=msg,qr_name=str(name)+'.png',qr_route=qr_route)
 if __name__=='__main__':
-    app.run(host='0.0.0.0')
+    app.run()
